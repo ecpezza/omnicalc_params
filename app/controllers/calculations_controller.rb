1b36a339db19a29572c2ec9615c80ebabacf8668
Elizabeth C. Pezza <ecpezza@users.noreply.github.com>
2017-04-24 22:29:42 -0500

    End of working period, April 24

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -12,12 +12,23 @@ class CalculationsController < ApplicationController
       render("calculations/flex_square_root.html.erb")
     end
 
-    #def monthly_payment
-    #  @user_provided_basis_points = params["basis_points"].to_f
-    #  @user_provided_term = params["number_of_years"].to_f
-    #  @user_provided_pv = params["present_value"].to_f
-    #  @monthlypayment =
-    #end#
+    def flex_monthly_payment
+      @user_provided_basis_points = params["basis_points"].to_f
+      @user_provided_term = params["number_of_years"].to_f
+      @user_provided_pv = params["present_value"].to_f
+      @monthlypayment = (@user_provided_pv*(@user_provided_basis_points/10000/12))/(1-((1+(@user_provided_basis_points/10000/12))**((-(@user_provided_term*12)))))
+      render("calculations/flex_monthly_payment.html.erb")
+      # (1) number to currency and rounding issues here and in form
+    end
+
+    def flex_random_number
+      @user_provided_min = params["min"]
+      @user_provided_max = params["max"]
+      @user_generated_array = []
+      @random_number = rand()
+      render("calculations/flex_random_number.html.erb")
+      # (2) figure out how to generate random within boundaries  here and in form
+    end
 
     def square_form
       render("calculations/square_form.html.erb")
@@ -27,6 +38,39 @@ class CalculationsController < ApplicationController
       @user_provided_number = params["number_to_square"].to_f #number_to_square key created in form name
       @squared_number=@user_provided_number**2
       render("calculations/square.html.erb")
+    end
+
+    def square_root_form
+      render("calculations/square_root_form.html.erb")
+    end
+
+    def square_root
+      @user_provided_number = params["number_to_square_root"].to_f
+      @square_root = @user_provided_number**0.5
+      render("calculations/square_root.html.erb")
+    end
+
+    def monthly_payment_form
+      render("calculations/monthly_payment_form.html.erb")
+    end
+
+    def monthly_payment
+      @user_provided_APR = params["APR"].to_f
+      @user_provided_term = params["term"].to_f
+      @user_provided_pv = params["principal"].to_f
+      @monthlypayment = ((@user_provided_pv*(@user_provided_APR/100/12))/(1-((1+(@user_provided_APR/100/12))**((-(@user_provided_term*12))))))
+      render("calculations/monthly_payment.html.erb")
+    end
+
+    def random_number_form
+      render("calculations/random_number_form.html.erb")
+    end
+
+    def random_number
+      @user_provided_min = params["min"].to_f
+      @user_provided_max = params["max"].to_f
+      @random_number = rand()
+      render("calculations/random_number.html.erb")
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,26 @@ Rails.application.routes.draw do
 
   get("/flexible/square_root/:the_number",{:controller => "calculations",:action => "flex_square_root"})
 
-  get("/flexible/payment/:basis_points/:number_of_years/:present_value",{:controller => "calculations",:action => "monthly_payment"})
+  get("/flexible/payment/:basis_points/:number_of_years/:present_value",{:controller => "calculations",:action => "flex_monthly_payment"})
+
+  get("/flexible/random/:min/:max",{:controller => "calculations",:action => "flex_random_number"})
 
   get("/square/new",{:controller => "calculations",:action => "square_form"})  #static route
 
   get("/square/results",{:controller => "calculations",:action => "square"}) #query string adds automatically
   #/square/results?number_to_square=5
+
+  get("/square_root/new",{:controller => "calculations",:action => "square_root_form"})
+
+  get("/square_root/results",{:controller => "calculations",:action => "square_root"})
+
+  get("/payment/new",{:controller => "calculations",:action => "monthly_payment_form"})
+
+  get("/payment/results",{:controller => "calculations",:action => "monthly_payment"})
+
+  get("/random/new",{:controller => "calculations",:action => "random_number_form"})
+
+  get("/random/results",{:controller => "calculations",:action => "random_number"})
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
